7e86373d1a2ce1e5c4f9eef65ad766f52d0285bb
jingchunzhu <jingchunzhu@gmail.com>
2018-08-11 00:05:52 -0700

    version number bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 print(find_packages())
 setup(
     name="xenaPython",
-    version="1.0.8",
+    version="1.0.9",
     packages=find_packages(),
     include_package_data=True,
     author = '@jingchunzhu, @acthp',

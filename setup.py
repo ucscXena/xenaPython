a10e1c22d2a4428ac5c1653bb5492d8205de6de0
jingchunzhu <jingchunzhu@gmail.com>
2020-04-03 11:38:02 -0700

    add convert.py for converting .loom .h5ad files to matrix files

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from setuptools import setup, find_packages
 print(find_packages())
 setup(
     name="xenaPython",
-    version="1.0.11",
+    version="1.0.12",
     packages=find_packages(),
     include_package_data=True,
     author = '@jingchunzhu, @acthp',
-    author_email = 'jzhu@soe.ucsc.com',
+    author_email = 'craft@soe.ucsc.com',
     description = 'XENA python API',
     url = 'https://github.com/ucscXena/xenaPython',
     keywords = ['xena', 'ucsc', 'xenaAPI', 'xenaPython'],
     license='Apache 2.0',
+    extras_require={
+        "convert": ["scanpy", "loom", "cellbrowser"],
+    }
 )

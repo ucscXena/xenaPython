6403c9ab327295d345084684a0c5b46759d7cd3e
jingchunzhu <jingchunzhu@gmail.com>
2020-04-07 16:50:42 -0700

    version 1.0.13

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 print(find_packages())
 setup(
     name="xenaPython",
-    version="1.0.12",
+    version="1.0.13",
     packages=find_packages(),
     include_package_data=True,
     author = '@jingchunzhu, @acthp',

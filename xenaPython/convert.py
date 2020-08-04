3826de0d7fdaa448861463ebe0f834fcd4362779
jingchunzhu <jingchunzhu@gmail.com>
2020-08-04 15:27:43 -0700

    add conversion function for starfish generated loom files

--- xenaPython/convert.py
+++ xenaPython/convert.py
@@ -32,7 +32,7 @@ def buildsjson_phenotype(output, cohort, label = None):
     json.dump(J, fout, indent = 4)
     fout.close()
 
-def anndataMatrixToTsv(adata, matFname):
+def anndataMatrixToTsv(adata, matFname, transpose = True):
     """
     write adata expression matrix to .tsv file"
     """
@@ -41,23 +41,31 @@ def anndataMatrixToTsv(adata, matFname):
 
     mat = adata.X
     var = adata.var
+    obs = adata.obs
 
-    # Transposing matrix, as scanpy has the samples on the rows
-    mat = mat.T
-
+    # Transposing matrix, has the samples on the rows: scanpy
+    # Do not transpose, has the cells on the rows: starfish
+    if (transpose):
+        mat = mat.T
     if scipy.sparse.issparse(mat):
         mat = mat.tocsr() # makes writing to a file ten times faster, thanks Alex Wolf!
 
     ofh = open(matFname, "w")
 
-    sampleNames = adata.obs.index.tolist()
+    if (transpose):
+        sampleNames = obs.index.tolist()
+    else:
+        sampleNames = var.index.tolist()
     ofh.write("gene\t")
     ofh.write("\t".join(sampleNames))
     ofh.write("\n")
 
-    genes = var.index.tolist()
-
+    if (transpose):
+        genes = var.index.tolist()
+    else:
+        genes = obs.genes.tolist()
     print("Writing %d genes in total" % len(genes))
+
     for i, geneName in enumerate(genes):
         if i % 2000==0:
             print("Wrote %d genes" % i)
@@ -73,7 +81,7 @@ def anndataMatrixToTsv(adata, matFname):
 
     ofh.close()
 
-def adataToXena(adata, path, studyName):
+def adataToXena(adata, path, studyName, transpose = True):
     """
     Given a scanpy anndata (adata) object, write dataset to a dataset directory under path.
     """
@@ -87,9 +95,9 @@ def adataToXena(adata, path, studyName):
     if isfile(matName):
         overwrite  = input("%s already exists. Overwriting existing files? Yes or No: " % matName)
         if overwrite.upper() == "YES":
-            anndataMatrixToTsv(adata, matName)
+            anndataMatrixToTsv(adata, matName, transpose)
     else:
-        anndataMatrixToTsv(adata, matName)
+        anndataMatrixToTsv(adata, matName, transpose)
     
     # build expression data .json file
     buildsjson_scRNA_geneExp(matName, studyName)
@@ -97,7 +105,10 @@ def adataToXena(adata, path, studyName):
     # build meta data (phenotype data) file
     metafile = 'meta.tsv'
     metaName = join(path, metafile)
-    adata.obs.to_csv(metaName, sep='\t')
+    if (transpose):
+        adata.obs.to_csv(metaName, sep='\t')
+    else:
+        adata.var.to_csv(metaName, sep='\t')
 
     # build meta data .json file
     buildsjson_phenotype(metaName, studyName)
@@ -113,14 +124,26 @@ def adataToXena(adata, path, studyName):
         data.to_csv(join(path, spatial_coord_file), sep='\t')
         buildsjson_phenotype(join(path, spatial_coord_file), studyName, label)
 
+def scanpyLoomToXena(matrixFname, path, studyName, transpose = True):
+    """
+    Given a scanpy loom file, write dataset to a dataset directory under path.
+    Transposing matrix needed, as scanpy has the samples on the rows
+    """
+    loomToXena(matrixFname, path, studyName, transpose)
 
-    
-def loomToXena(matrixFname, path, studyName):
+def starfishLoomToXena(matrixFname, path, studyName, transpose = False):
+    """
+    Given a starfish loom file, write dataset to a dataset directory under path.
+    Transposing matrix not needed, as starfish has the cells on the rows
+    """
+    loomToXena(matrixFname, path, studyName, transpose)
+
+def loomToXena(matrixFname, path, studyName, transpose = True):
     """
     Given a loom file, write dataset to a dataset directory under path.
     """
     adata = sc.read(matrixFname, first_column_names=True)
-    adataToXena(adata, path, studyName)
+    adataToXena(adata, path, studyName, transpose)
 
 def h5adToXena(matrixFname, path, studyName):
     """
